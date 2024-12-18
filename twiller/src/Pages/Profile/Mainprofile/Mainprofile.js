5187883185e5e1ae5b5408b14b5a55ef7e45e27d
Atharv-28 <atharvmt@outlook.com>
2024-12-18 15:22:41 +0530

    Fixing bugs

--- twiller/src/Pages/Profile/Mainprofile/Mainprofile.js
+++ twiller/src/Pages/Profile/Mainprofile/Mainprofile.js
@@ -103,32 +103,6 @@ const Mainprofile = ({ user }) => {
         setisloading(false);
       });
   };
-  // const data = [
-  //   {
-  //     _id: "1",
-  //     name: "Jane Doe",
-  //     username: "jane_doe",
-  //     profilePhoto: "https://example.com/profiles/jane.jpg",
-  //     post: "Exploring the new features in JavaScript! 🚀 #coding #JavaScript",
-  //     photo: "https://example.com/posts/javascript.png",
-  //   },
-  //   {
-  //     _id: "2",
-  //     name: "John Smith",
-  //     username: "johnsmith",
-  //     profilePhoto: "https://example.com/profiles/john.jpg",
-  //     post: "Just finished a great workout session! 💪 #fitness #health",
-  //     photo: "https://example.com/posts/workout.png",
-  //   },
-  //   {
-  //     _id: "3",
-  //     name: "Alice Johnson",
-  //     username: "alicejohnson",
-  //     profilePhoto: "https://example.com/profiles/alice.jpg",
-  //     post: "Loving the new features in CSS! #webdevelopment #design",
-  //     photo: "https://example.com/posts/css.png",
-  //   },
-  // ];
   return (
     <div>
       <ArrowBackIcon className="arrow-icon" onClick={() => navigate("/")} />
